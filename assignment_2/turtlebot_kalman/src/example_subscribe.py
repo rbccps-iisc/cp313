ca82567251c55cdebfffc1631feb80dec10499b7
Rakshit Ramesh <rakshitadmar@gmail.com>
2019-09-18 10:11:05 +0530

    Updated example

--- assignment_2/turtlebot_kalman/src/example_subscribe.py
+++ assignment_2/turtlebot_kalman/src/example_subscribe.py
@@ -12,6 +12,11 @@ import numpy as np
 import time
 
 
+beacon_msg_flag = 0
+vel_msg_flag  = 0
+beacon_msg = None
+vel_msg = None
+
 ''' ROS Node initiation '''
 rospy.init_node('turtlebot_example_node')
 rate = rospy.Rate(100)
@@ -19,10 +24,28 @@ rate = rospy.Rate(100)
 
 ''' Turtlebot noisy velocity callback '''
 def noisy_vel_cb(data):
-    print(data)
+    global vel_msg
+    global vel_msg_flag
+    vel_msg = data
+    vel_msg_flag = 1
+    pass
+
+''' Turtlebot noisy velocity callback '''
+def beacon_cb(data):
+    global beacon_msg
+    global beacon_msg_flag
+    beacon_msg = data
+    beacon_msg_flag = 1
 
 ''' Subscribers '''
 rospy.Subscriber('/noisy_vel', Twist, noisy_vel_cb)
+rospy.Subscriber('/beacon', BeaconMsg, beacon_cb)
 
 
-rospy.spin()
+while(not rospy.is_shutdown()):
+
+    if(vel_msg_flag == 1):
+        print(vel_msg)
+    if(beacon_msg_flag == 1):
+        print(beacon_msg)
+
